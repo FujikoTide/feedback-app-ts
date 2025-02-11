8ce4e9deae675e4655442aa508272b9cf135100d
Robin Mukherjee <robin@otaking.co.uk>
2025-02-11 21:15:04 +0000

    don't dedupe react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from './components/Header.tsx'
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
+  )
+}
